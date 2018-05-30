31395420e60f853f291b6176d1a2354750e45ed1
AnNguyen <nguyenan.public@gmail.com>
2018-05-30 11:37:09 +0700

    return json v1.3

--- examples/getAccountMediasByUsername.php
+++ examples/getAccountMediasByUsername.php
@@ -12,8 +12,8 @@ if(isset($_GET["u"]))
 $instagram = new \InstagramScraper\Instagram();
 $medias = $instagram->getMedias($user_name, 25);
 
-$out = array_values($medias[0]);
-echo json_encode($out);
+//$out = array_values($medias[0]);
+echo json_encode($medias[0]);
 
 // Let's look at $media
 /*$media = $medias[0];
